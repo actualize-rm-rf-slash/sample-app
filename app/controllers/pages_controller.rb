28c0d5640c3537ac92fc69241c5c4bad5d7b6fe9
Peter Jang <peter.x.jang@gmail.com>
2018-03-04 09:52:27 -0600

    Add test route with examples for xml and html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def hello_method
     render json: {message: "Hello!!!"}
   end
+
+  def test_method
+    # render xml: {a: "b", name: "Peter", :email => "peter@email.com"}
+    render html: "<h1>Hello</h1>".html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/hello_url" => "pages#hello_method"
+  get "/test_url" => "pages#test_method"
 end
