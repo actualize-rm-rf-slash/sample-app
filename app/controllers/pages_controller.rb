412e86c9fc4a698529305b820f9d7b6b88b72aa4
Peter Jang <peter.x.jang@gmail.com>
2018-03-04 10:05:53 -0600

    Add time route with current time formatted with strftime

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,8 @@ class PagesController < ApplicationController
     # render xml: {a: "b", name: "Peter", :email => "peter@email.com"}
     render html: "<h1>Hello</h1>".html_safe
   end
+
+  def time_method
+    render json: {time: Time.now.strftime("%a, %e %b %Y %H:%M:%S %z")}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/hello_url" => "pages#hello_method"
   get "/test_url" => "pages#test_method"
+  get "/time_url" => "pages#time_method"
 end
